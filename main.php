f4c9d180614ef510147f35aae0304b5bb9084844
FootLettuce26 <caleb.weigt@cuw.edu>
2024-09-14 16:57:23 -0500

    not working
    attempting merge conflict

--- main.php
+++ main.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/production.css">
-    <title>Get Your Sh*t Done</title>
+    <title>Get Some Sh*t Done</title>
 </head>
-<body>    
-<h1>To-Do</h1>
+
+<body>
+    <div>
+        <h1>To-Do</h1>
+    </div>
+    <div>
+        <p><?php echo "Welcome to the list" ?></p>
+    </div>
 </body>
+
 </html>
